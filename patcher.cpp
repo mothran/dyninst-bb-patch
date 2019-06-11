f01cb9dbd848d0798d26ac71a2278ab9287a364b
mothran <w.parker.thompson@gmail.com>
2019-06-11 14:45:58 -0700

    Convert indentation

--- patcher.cpp
+++ patcher.cpp
@@ -28,14 +28,14 @@ int main(int argc, char *argv[])
     auto output_path = std::string(argv[2]);
     std::string runtime = "libruntime.so";
 
-	BPatch bpatch;
-	BPatch_binaryEdit *appBin = bpatch.openBinary(input_path.c_str(), false);
+    BPatch bpatch;
+    BPatch_binaryEdit *appBin = bpatch.openBinary(input_path.c_str(), false);
 
-	if (!appBin->loadLibrary(runtime.c_str())) {
+    if (!appBin->loadLibrary(runtime.c_str())) {
         std::cerr << "Failed to load runtime: " << runtime << std::endl;
-		return 1;
-	}
-	BPatch_image *appImg = appBin->getImage();
+        return 1;
+    }
+    BPatch_image *appImg = appBin->getImage();
 
     std::vector<BPatch_function *> funcs;
     appImg->findFunction("bb_callback", funcs);
@@ -46,18 +46,18 @@ int main(int argc, char *argv[])
     
     BPatch_function *instBBCallback = funcs[0];
 
-	std::vector<BPatch_module *> mods;
-	if (!appImg->getModules(mods)) {
+    std::vector<BPatch_module *> mods;
+    if (!appImg->getModules(mods)) {
         std::cerr << "Failed to get module list for: " << input_path << std::endl;
-		return 1;
-	}
+        return 1;
+    }
 
-	for (BPatch_module *mod : mods)
-	{
+    for (BPatch_module *mod : mods)
+    {
         char buf[4096];
-		memset(buf, 0, sizeof(buf));
-		mod->getName(buf, sizeof(buf) - 1);
-		std::string mod_name = buf;
+        memset(buf, 0, sizeof(buf));
+        mod->getName(buf, sizeof(buf) - 1);
+        std::string mod_name = buf;
 
         if (mod_name == runtime) {
             continue;
@@ -65,54 +65,54 @@ int main(int argc, char *argv[])
         std::cout << "Module: " << mod_name << std::endl;
 
         std::vector<BPatch_function *> funcs;
-		if (!mod->getProcedures(funcs)) {
+        if (!mod->getProcedures(funcs)) {
             std::cerr << "Failed to get functions for module: " << mod_name << std::endl;
-			return 1;
-		}
+            return 1;
+        }
 
-		for (BPatch_function *func : funcs) {
-			std::string func_name = func->getName();
+        for (BPatch_function *func : funcs) {
+            std::string func_name = func->getName();
 
-			if(func_name == "_start") {
+            if(func_name == "_start") {
                 continue;
             }
 
             std::cout << "func: " << func_name << std::endl;
-			BPatch_flowGraph *CFG = func->getCFG();
-			if (!CFG) {
+            BPatch_flowGraph *CFG = func->getCFG();
+            if (!CFG) {
                 continue;
             }
 
-			BPatch_Set<BPatch_basicBlock *> bbs;
-			if (!CFG->getAllBasicBlocks(bbs)) {
+            BPatch_Set<BPatch_basicBlock *> bbs;
+            if (!CFG->getAllBasicBlocks(bbs)) {
                 std::cerr << "Failed to get basicblocks for: " << func_name << " in: " << mod_name << std::endl;
-				continue;
-			}
+                continue;
+            }
 
             for(BPatch_basicBlock *bb : bbs) {
-				uint32_t start_addr = (uint32_t) bb->getStartAddress();
+                uint32_t start_addr = (uint32_t) bb->getStartAddress();
 
-				BPatch_point *bbEntry = bb->findEntryPoint();
-				if (!bbEntry) {
+                BPatch_point *bbEntry = bb->findEntryPoint();
+                if (!bbEntry) {
                     continue;
                 }
 
-				std::vector<BPatch_snippet *> instArgs;
-				BPatch_constExpr exprCompID(start_addr);
-				instArgs.push_back(&exprCompID);
-				BPatch_funcCallExpr instBBExpr(*instBBCallback, instArgs);
+                std::vector<BPatch_snippet *> instArgs;
+                BPatch_constExpr exprCompID(start_addr);
+                instArgs.push_back(&exprCompID);
+                BPatch_funcCallExpr instBBExpr(*instBBCallback, instArgs);
 
-				BPatchSnippetHandle *handle = appBin->insertSnippet(instBBExpr,
+                BPatchSnippetHandle *handle = appBin->insertSnippet(instBBExpr,
                                                                     *bbEntry,
                                                                     BPatch_callBefore,
                                                                     BPatch_lastSnippet);
-				if(!handle) {
+                if(!handle) {
                     std::cerr << "Failed to instrument " << func_name << " in mod: " << mod_name << " at address 0x" << std::hex << start_addr << std::endl;
-				}
+                }
             }
         }
     }
 
-	appBin->writeFile(output_path.c_str());
+    appBin->writeFile(output_path.c_str());
     return 0;
 }
